a3a8dfdc594a640da6513d5c4be5c12b1fd8860a
GREEENFONTS <58170214+GREENFONTS@users.noreply.github.com>
2020-04-22 20:06:48 +0100

    Revert "init"

--- src/component/Nav.js
+++ src/component/Nav.js
@@ -3,54 +3,54 @@ import { Link } from 'react-router-dom';
 import '../App.css'
 
 function Nav() {
+    
+    return(
+            <div className=" container-fluid bg-info">
+                    <nav className="navbar navbar-expand-lg  navbar-light" id="nav">
+                        <a className="navbar-brand pull-right" href="#" id="name">GREENFONTS</a>
+                        <button className="navbar-toggler" type="button" data-toggle="collapse"
+                            data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                            aria-label="Toggle navigation">
+                            <span className="navbar-toggler-icon"></span>
+                        </button>
 
-return(
-<div className=" container-fluid bg-info">
-<nav className="navbar navbar-expand-lg  navbar-light" id="nav">
-<a className="navbar-brand pull-right" href="#" id="name">GREENFONTS</a>
-<button className="navbar-toggler" type="button" data-toggle="collapse"
-data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-aria-label="Toggle navigation">
-<span className="navbar-toggler-icon"></span>
-</button>
+                        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav  mr-auto " id="nav-list">
+                            <Link to="/">
+                                <li className="nav-item active">
+                                    HOME
+                                </li>
+                            </Link>
+                            <Link to="/Resume">
+                                <li className="nav-item active">
+                                    Resume
+                                </li>
+                            </Link> 
+                            <Link to="/Service">
+                                <li className="nav-item active">
+                                    SERVICES
+                                </li>
+                            </Link>
+                            <Link to="/Blog">
+                                <li className="nav-item active">
+                                   BLOG
+                                </li>
+                            </Link>
+                            <Link to="/Contact">
+                                <li className="nav-item active">
+                                    CONTACT
+                                </li>
+                            </Link>
+                               
+                            </ul>
+                        </div>
+                    </nav>
+                </div>
 
-<div className="collapse navbar-collapse" id="navbarSupportedContent">
-<ul className="navbar-nav  mr-auto " id="nav-list">
-<Link to="/">
-<li className="nav-item active">
-HOME
-</li>
-</Link>
-<Link to="/Resume">
-<li className="nav-item active">
-Resume
-</li>
-</Link> 
-<Link to="/Service">
-<li className="nav-item active">
-SERVICES
-</li>
-</Link>
-<Link to="/Blog">
-<li className="nav-item active">
-BLOG
-</li>
-</Link>
-<Link to="/Contact">
-<li className="nav-item active">
-CONTACT
-</li>
-</Link>
-
-</ul>
-</div>
-</nav>
-</div>
-
-
-
-)
-
-}
+               
+           
+        )
+    
+    }
 
 export default Nav;
